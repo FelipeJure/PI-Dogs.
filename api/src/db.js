f6176e261d7e4ebd2d4cee620418bb9580d547c6
FelipeJure <felipe.jure05@gmail.com>
2022-10-25 14:16:58 +0200

    change deploy to Heroku

--- api/src/db.js
+++ api/src/db.js
@@ -4,17 +4,49 @@ const fs = require('fs');
 const fetch = require('node-fetch')
 const path = require('path');
 const {
-  PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE
+  // variables de Railway
+  PGUSER, PGPASSWORD, PGHOST, PGDATABASE,
+  // variables de Heroku
+  DB_USER, DB_PASSWORD, DB_HOST
 } = process.env;
 
 let sequelize =
   process.env.NODE_ENV === "production"
-    ? new Sequelize(`${process.env.DATABASE_URL}`)
-    : new Sequelize(
-        `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`,
-        { logging: false, native: false }
-      );
-// chau heroku
+  // en Railway
+    // ? new Sequelize(`${process.env.DATABASE_URL}`)
+    // : new Sequelize(
+    //     `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`,
+    //     { logging: false, native: false }
+      // );
+// en heroku
+  ? 
+    new Sequelize({
+      database: DB_NAME,
+      dialect: "postgres",
+      host: DB_HOST,
+      port: 5432,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      pool: {
+        max: 3,
+        min: 1,
+        idle: 10000,
+      },
+      dialectOptions: {
+        ssl: {
+          require: true,
+          // Ref.: https://github.com/brianc/node-postgres/issues/2009
+          rejectUnauthorized: false,
+        },
+        keepAlive: true,
+      },
+      ssl: true,
+    })
+  : 
+    new Sequelize(
+      `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`,
+      { logging: false, native: false }
+    );
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
 //   logging: false, // set to console.log to see the raw SQL queries
